63c33232d57bc7018784dcd1a1841e1ed761c1b7
Bill <wrconner92@gmail.com>
2015-10-19 14:56:11 -0400

    Finished Trail class and worked on Main

--- src/Main.java
+++ src/Main.java
@@ -5,30 +5,56 @@ public class Main {
 
     public static void main(String[] args) { new Main();}
 
+    private static final int NUMTRAILS = 4;
+
     private MClock clock;
     private Stack<Hiker> hikerStack;
+    private Trail[] trails;
+    private Hiker hiker;
 
     public Main(){
         clock = new MClock();
         hikerStack = new Stack<Hiker>();
+        trails = new Trail[NUMTRAILS];
+
+        makeTrails();
 
         while(clock.getHour() < 22)
             runLoop();
+    }
+
+    private void makeTrails(){
+        for (int i = 0; i < NUMTRAILS; i++){
+            trails[i] = new Trail(i + 1);
+        }
     }
 
     private void runLoop(){
         Random random = new Random();
         int r = random.nextInt(10);
 
-        for (int i = 0; i < r; i ++) {
-            if (hikerStack.size() <= 10)
-                hikerStack.add(new Hiker());
-            else{
-                //TODO Add hikerStack to trail
-                hikerStack.clear();
-                hikerStack.add(new Hiker());
+        while(clock.getHour() <= 10) {
+            for (int i = 0; i < r; i++) {
+                if (hikerStack.size() < 10)
+                    hikerStack.add(new Hiker());
+                else {
+                    int c = random.nextInt(4);
+                    trails[c].addStack(hikerStack);
+                    hikerStack.clear();
+                    hikerStack.add(new Hiker());
+                }
+            }
+        }
+        if ((clock.getHour() == 7) && (clock.getMinuteTens() == 0) && (clock.getMinuteOnes() == 0)) { //10:00 AM
+            System.out.println("Hiker ID Numbers:");
+            for (int i = 0; i < NUMTRAILS; i++){
+                for (int j =0; j < 20; j++){
+                    //TODO pass hiker to logbook
+                    System.out.println(trails[i].releaseHiker().getIdNum());
+                }
             }
         }
         clock.updateTime();
+        System.out.println("Time: " + clock.getHour() + ":" + clock.getMinuteTens() + clock.getMinuteOnes());
     }
 }
--- src/Trail.java
+++ src/Trail.java
@@ -1,5 +1,27 @@
+import java.util.ArrayDeque;
+import java.util.Stack;
+
 /**
  * Created by bill on 10/16/15.
  */
 public class Trail {
+    private ArrayDeque<Hiker> hikerQueue;
+    private int trailName;
+
+    public Trail(int trailName) {
+        this.trailName =trailName;
+        hikerQueue = new ArrayDeque<Hiker>(100);
+    }
+
+    public void addStack(Stack<Hiker> hikerStack){
+        for (int i = 0; i < hikerStack.size(); i++)
+            hikerQueue.add(hikerStack.pop());
+    }
+
+    public Hiker releaseHiker(){
+        return hikerQueue.pop();
+    }
+
+    public int getTrailName(){ return trailName;}
+
 }
